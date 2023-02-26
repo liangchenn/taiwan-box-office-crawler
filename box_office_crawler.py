f34d482f5407d7953a4b264c328591de013a65c0
liangchenn <r08323022@ntu.edu.tw>
2023-02-26 05:56:43 +0000

    feat: add cli argparser for program

--- box_office_crawler.py
+++ box_office_crawler.py
@@ -1,9 +1,13 @@
+import argparse
 import logging
 
-from utils.crawler import BoxOfficeCrawler
+import arrow
 
-DATASET_ID = "box_office"
-TABLE_ID = "taiwan_movie_box_office"
+from utils.constant import DATASET_ID
+from utils.constant import TABLE_ID
+from utils.crawler import BoxOfficeCrawler
+from utils.date import validate_dateformat
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,13 +15,50 @@ logging.basicConfig(
 )
 
 
-if __name__ == "__main__":
+def make_parser():
 
-    start_date = "2023-02-15"
-    end_date = "2023-02-20"
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "-s", "--start_date", help="Start date of data to parse.", type=validate_dateformat
+    )
+    parser.add_argument(
+        "-e", "--end_date", help="Start date of data to parse.", type=validate_dateformat
+    )
+    parser.add_argument(
+        "-d", "--run_date", help="Target date of data to parse.", type=validate_dateformat
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
 
     logger = logging.getLogger(__name__)
     crawler = BoxOfficeCrawler()
+
+    # cli argments
+    parser = make_parser()
+    args = parser.parse_args()
+
+    start_date = args.start_date
+    end_date = args.end_date
+    run_date = args.run_date
+
+    # validate date arguments
+    if start_date and not end_date:
+        parser.error("end_ate not provided.")
+
+    if end_date and not start_date:
+        parser.error("start_date not provided.")
+
+    if run_date:
+        start_date = run_date
+        end_date = run_date
+
+    # default to 10D, 3D before current date
+    if not (start_date and end_date):
+        start_date = arrow.now().shift(days=-10).format("YYYY-MM-DD")
+        end_date = arrow.now().shift(days=-3).format("YYYY-MM-DD")
 
     try:
         crawler.run_crawler(
@@ -25,7 +66,7 @@ if __name__ == "__main__":
             end_date=end_date,
             dataset_id=DATASET_ID,
             table_id=TABLE_ID,
-            batch_upload_days=4,
+            batch_upload_days=7,
         )
         logger.info("done")
     except Exception as err:
--- utils/date.py
+++ utils/date.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentTypeError
+
 import arrow
 
 
@@ -7,3 +9,12 @@ def gen_date_list(start_date: str, end_date: str):
     e = arrow.get(end_date)
 
     return [d.format("YYYY-MM-DD") for d in arrow.Arrow.range("day", s, e)]
+
+
+def validate_dateformat(date: str):
+
+    try:
+        return arrow.get(date).format("YYYY-MM-DD")
+    except ValueError:
+        msg = f"Not a valid date format: {date:!r}"
+        raise ArgumentTypeError(msg)
